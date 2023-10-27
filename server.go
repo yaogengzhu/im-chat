4837edd37e8b91940c36245a8ab04b081dffd8ff
yaogengzhu <yaogengzhu@tencent.com>
2023-10-27 13:34:50 +0800

    用户强制下线

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -61,6 +62,9 @@ func (this *Server) Handler(conn net.Conn) {
 
 	user.UserOnline()
 
+	// 监听用户是否活跃的channel
+	isLive := make(chan bool)
+
 	// 接收客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
@@ -78,10 +82,25 @@ func (this *Server) Handler(conn net.Conn) {
 			msg := string(buf[:n-1]) // windows上多一个换行符
 			// 将得到的消息进行广播
 			user.DoMessage(msg)
+			isLive <- true
 		}
 	}()
 	// 当前handler阻塞
-	select {}
+
+	for {
+
+		select {
+		case <-isLive:
+			// 当前用户是活跃的，应该重置定时器
+			// 不做任何事情，为了激活select，更新下面的定时器
+		case <-time.After(time.Second * 10):
+			// fmt.Println("超时")
+			user.SendMsg("你被踢了\n")
+			// 销毁用的资源
+			close(user.C)
+			conn.Close()
+		}
+	}
 }
 
 func (this *Server) Start() {
